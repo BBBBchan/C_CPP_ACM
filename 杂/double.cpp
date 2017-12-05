07125a1b5266322e0532f7816fdeafe6f8fe5f4e
BBBBchan <sbysbysby123@gmail.com>
2017-12-05 13:24:56 +0800

    【

--- 杂/double.cpp
+++ 杂/double.cpp
@@ -3,6 +3,5 @@ int main(){
 	double i;
 	for(i = 0.000000;i != 0.5; i+=0.100000)
 	printf("%lf",i);
-	
 	return 0; 
 }
---
+++ 杂/scanf的讨论.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	while(scanf("(%d, %d%*s",&a,&b) != EOF){
+	printf("(%d, %d)",a,b);
+}
+	return 0;
+}
