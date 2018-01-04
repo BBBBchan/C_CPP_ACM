b31b0d69ac4628544ca883bac744f6293f59c6d7
BBchan <sbysbysby123@gmail.com>
2018-01-04 15:40:04 +0800

    12

--- 杂/字符串.cpp
+++ 杂/字符串.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-	char *s = "hello";
-	printf("%s\n",s );
+int main(int argc, char const *argv[])
+{	int m;
+	scanf("%d" , &m);
+	for(int i = 0; i < argc; i++)
+		printf("%s\n",argv[i] );
 	return 0;
 }
