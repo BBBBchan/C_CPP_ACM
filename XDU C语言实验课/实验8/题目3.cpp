3f36395fc3db2265f793003d9503a062392344f5
BBchan <sbysbysby123@gmail.com>
2018-01-08 23:02:35 +0800

    12

---
+++ XDU C语言实验课/实验8/题目3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int mystrcmp(char *a, char *b){
+	for(int i = 0; i < strlen(a) || i < strlen(b); i++){
+		if(a[i] > b[i])
+			return a[i] - b[i];
+		else if(a[i] < b[i])
+			return a[i] - b[i];
+	}
+	return 0;
+}
+int main(){
+	char a[100],b[100];
+	scanf("%s %s",a,b);
+	int k = mystrcmp(a,b);
+	printf("%d\n", k);
+	return 0;
+}
