1dfb8ba37d9c88cad113f5dfdf7d4091e1466e92
BBchan <sbysbysby123@gmail.com>
2018-08-07 14:28:41 +0800

    1

--- Vjudge/POJ_1003.cpp
+++ Vjudge/POJ_1003.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-char s[20];
-int letter[] = {2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,0,7,7,8,8,8,9,9,9,0};
+char s[100];
+int letter[] = {2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,0,7,7,8,8,8,9,9,9};
 
 int phone[100005] = {0};
 
@@ -31,20 +31,25 @@ int main(int argc, char const *argv[])
 		}
 	}
 	sort(phone,phone+t);
-	int count = 1,kase = 0;;
-	for(int i = 1; i <=t; i++){
+	int count = 1,kase = 0,i;
+	for(i = 1; i <t; i++){
 		if(phone[i] == phone[i-1])
 			count++;
 		else{
 			if(count > 1){
-				printf("%d-%d %d\n", phone[i-1]/10000, phone[i-1]%10000, count);
+				printf("%03d-%04d %d\n", phone[i-1]/10000, phone[i-1]%10000, count);
 				kase = 1;
 			}
 			count = 1;
 		}
 		}
+		if(count > 1){
+			printf("%03d-%04d %d\n", phone[i-1]/10000, phone[i-1]%10000, count);
+			kase = 1;
+		}
+
 	if(!kase)
-		printf("No duplicates\n");
+		printf("No duplicates.\n");
 	return 0;
 		
 }
