03feeedf99f362dd9c1346affd5b5f164e9e0bac
BBchan <sbysbysby123@gmail.com>
2018-08-09 00:28:12 +0800

    1

---
+++ Vjudge/cf1.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int n,a,b,c,d,i;
+int cmp(int a, int b){
+	return a > b;
+}
+int main(int argc, char const *argv[])
+{
+	scanf("%d", &n);
+	int sum[n];
+	for(i = 0; i < n; i++){
+		scanf("%d %d %d %d", &a, &b, &c, &d);
+		sum[i] = a+b+c+d;
+	}
+	int ans = sum[0];
+	sort(sum,sum+n,cmp);
+	for(int i = 0; i < n; i++)
+		printf("%d ", sum[i]);
+	printf("\n");
+	for(i = 0; i < n; i++){
+		if(sum[i] == ans)
+			break;
+	}
+	printf("%d", i+1);
+	return 0;
+}
