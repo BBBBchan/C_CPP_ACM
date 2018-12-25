fc0a7eea06c5cbae758781e778b88f91fe186d82
BB CHAN <sbysbysby123@gmail.com>
2018-12-25 19:42:15 +0800

    1

---
+++ 数据结构上机/7/3.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+int n, m;
+queue <int> qe;
+int vis[120] = {0};
+int a[120][120];
+void bfs(int i){
+	int cur, temp;
+	while(!qe.empty()){
+		cur = qe.front();
+		if(vis[cur] == 0){
+			printf("%d ", cur+1);
+			vis[cur] = 1;
+			qe.pop();
+			for(int j = 0; j < n; j++){
+				if(a[cur][j] == 1)
+					qe.push(j);
+			}
+		}
+		else
+			qe.pop();
+	}
+}
+int main(int argc, char const *argv[])
+{
+	scanf("%d %d", &n, &m);
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			scanf("%d", &a[i][j]);
+	for(int i = 0; i < n; i++){
+		if(vis[i] == 0){
+			qe.push(i);
+			bfs(i);
+		}
+	}
+	return 0;
+}
---
+++ 数据结构上机/7/4.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <queue>
+#include <vector>
+using namespace std;
+struct edge
+{
+	int to;
+	int len;
+};
+queue <int> qe;
+vector < vector<edge>> G(120);
+int n, m;
+int vis[120] = {0};
+void bfs(int i){
+	int cur;
+	while(!qe.empty()){
+		cur = qe.front();
+		if(vis[cur] == 0){
+			vis[cur] = 1;
+			qe.pop();
+			printf("%d ", cur+1);
+			for(int j = 0; j < G[cur].size(); j++){
+				if(vis[G[cur][j].to] == 0)
+					qe.push(G[cur][j].to);
+			}
+		}
+		else
+			qe.pop();
+	}
+}
+int main(int argc, char const *argv[])
+{
+	scanf("%d %d", &n, &m);
+	for(int i = 0; i < m; i++){
+		edge r1, r2;
+		scanf("%d %d %d", &r2.to, &r1.to, &r1.len);
+		r2.len = r1.len;
+		G[r2.to].push_back(r1);
+		G[r1.to].push_back(r2);
+	}
+	for(int i = 0; i < n; i++){
+		if(vis[i] == 0){
+			qe.push(i);
+			bfs(i);
+		}
+	}
+	return 0;
+}
+/*
+6 9
+0 1 20
+0 2 16
+1 4 13
+1 2 15
+1 5 28
+5 4 12
+4 3 12
+2 3 14
+5 3 12
+*/
