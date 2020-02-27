27e2f020517f51c5c814b2f094edfc1d883e5881
BBchan <sbysbysby123@gmail.com>
2020-02-27 10:28:45 +0800

    Create 1.cpp

---
+++ 算法设计与程序分析/1.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int a[1000];
+int main(int argc, char const *argv[])
+{
+	int n, x;
+	scanf("%d %d", &n, &x);
+	for(int i = 0; i < n; i++){
+		scanf("%d", &a[i]);
+	}
+	sort(a,a+n);
+	int ansl = -1, ansr = -1, kase = 0;
+	for(int i = 0; i < n; i++){
+		int left = i, right = n-1;
+		if(kase){break;}
+		while(right > left){
+			int mid = left + (right-left)/2;
+			if(a[i] + a[mid] > x){
+				right = mid-1;
+			}
+			else if(a[i] + a[mid] < x){
+				left = mid+1;
+			}
+			else{
+				ansl = i;
+				ansr = mid;
+				kase = 1;
+				break;
+			}
+			// printf("%d %d %d\n", i, left,right);
+		}
+		// printf("%d %d\n", ansl, ansr);
+	}
+	printf("%d %d\n", ansl, ansr);
+	return 0;
+}
